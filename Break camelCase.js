9a40640f2629d1f69635e76a5452ddf2e9a7587a
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2022-05-04 23:54:36 +0800

    Update Break camelCase.js

--- Break camelCase.js
+++ Break camelCase.js
@@ -7,43 +7,18 @@ Example:
 ""             =>  ""
 */
 
-
-
-
+function isUpperCase(str) {
+    return str === str.toUpperCase();
+}
 function solution(string) {
-    // String.prototype.isUpperCase = function(){
-    //     return this.valueOf().toUpperCase() === this.valueOf;
-    // }
 
     let newArr = string.split("")
     // console.log(newArr);
-    return string.map(e => { 
-        console.log(typeof(e));
-        console.log(isUpperCase(e));
-        // isUpperCase(e) ? (" " + e) : e
+    return newArr.map(e => { 
         if (isUpperCase(e)){
             return " " + e;
         } else {
             return e;
         }
-    })
+    }).join("")
 }
-
-function isUpperCase(str) {
-    return str === str.toUpperCase();
-}
-
-console.log(solution("camelCasingTest"));
-
-// let test = "caseClosed"
-// let testArr = test.split("");
-// let result = testArr.map((e,i) => {
-//     console.log("String = " + e + ", Index = " + i + ", isUpper? " + isUpperCase(e));
-//     // console.log();
-//     if(isUpperCase(e)){
-//         testArr.splice(i,0," ");
-//     }
-// })
-// console.log(testArr)
-
-
