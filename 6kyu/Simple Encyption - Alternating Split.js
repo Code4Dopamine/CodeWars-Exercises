79d3e903e86772342422da94e08b3e3ebc8d8960
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2022-06-03 16:44:09 +0800

    Completed Simple Encryption - Alternating Split

--- 6kyu/Simple Encyption - Alternating Split.js
+++ 6kyu/Simple Encyption - Alternating Split.js
@@ -15,28 +15,61 @@ If the string S is an empty value or the integer N is not positive, return the f
 */
 
 function encrypt(text, n) {
-    function reOrder(t) {
-        let temp = []
-        for(let i = 1; i <= t.length; i+=2){
-          temp.push(t[i])
-        }
-        for(let j = 0; j <= t.length-1; j+=2){
-            temp.push(t[j])
-        }
-        return temp.join("")
+    if (text === null || n < 0){
+        return text
+      }
+  function reOrder(t) {
+    let temp = [];
+    for (let i = 1; i <= t.length; i += 2) {
+      temp.push(t[i]);
     }
+    for (let j = 0; j <= t.length - 1; j += 2) {
+      temp.push(t[j]);
+    }
+    return temp.join("");
+  }
 
-    let encrypt = text
-    for (let i = 0; i < n; i++){
-        encrypt = reOrder(encrypt)
-    }
-    console.log(encrypt)
+  let encrypt = text;
+  for (let i = 0; i < n; i++) {
+    encrypt = reOrder(encrypt);
+  }
+//   console.log(encrypt);
+    return encrypt
 }
 
 function decrypt(encryptedText, n) {
-    const mid = encryptedText/2
+    if(n < 0){
+        return encryptedText
+      }
+
+    function decrypter(t) {
+        const mid = t.length / 2;
+        const sect1 = t.split("").slice(mid) //Even, starting index 0
+        const sect2 = t.split("").slice(0,mid) //Odd Index, depleted to add to "sect1" array
+        
+        let result = sect1.reduce((init, curr) => {
+            init.push(curr);
+            if(sect2.length !== 0){
+                init.push(sect2.shift())
+            }
+            return init
+        },[])
+
+        return result.join("")
+    }
+
+  let decryptText = encryptedText
+  for (let i = 0; i < n; i++) {
+      decryptText = decrypter(decryptText);
+  }
+//   console.log(decryptText)
+  return decryptText
 }
 
-encrypt("012345",1)
-encrypt("01234",3)
+//TEST
 
+encrypt("012345", 2); //304152
+encrypt("01234", 3); //20314
+
+decrypt("304152", 2);
+decrypt("20314", 3);
