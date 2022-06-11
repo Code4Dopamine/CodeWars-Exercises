9419c9a8ebdc86ca4df76ac9b9380cf6c823d31d
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2022-06-11 23:31:19 +0800

    Create Bijective Binary.js

---
+++ 6kyu/Bijective Binary.js
@@ -0,0 +1,13 @@
+/*
+Create methods to convert between Bijective Binary strings and integers and back.
+
+A bijective binary number is represented by a string composed only of the characters 1 and 2. Positions have the same value as in binary numbers, a string of repeated 1's has the same value in binary and bijective binary. As there is no symbol for zero it is represented by an empty string.
+
+Every non negative integer has a unique representation unlike binary where 1 = 0001.
+
+https://en.wikipedia.org/wiki/Bijective_numeration
+*/
+
+function bijective(){
+    
+}
