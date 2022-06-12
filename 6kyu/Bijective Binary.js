1d40b245a3411bf72212304de85bf792e1366a6b
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2022-06-12 08:39:32 +0800

    Update Bijective Binary.js
    Completed integer to bijective binary conversion

--- 6kyu/Bijective Binary.js
+++ 6kyu/Bijective Binary.js
@@ -1,13 +1,33 @@
 /*
 Create methods to convert between Bijective Binary strings and integers and back.
-
 A bijective binary number is represented by a string composed only of the characters 1 and 2. Positions have the same value as in binary numbers, a string of repeated 1's has the same value in binary and bijective binary. As there is no symbol for zero it is represented by an empty string.
 
 Every non negative integer has a unique representation unlike binary where 1 = 0001.
-
 https://en.wikipedia.org/wiki/Bijective_numeration
+
+0 = ""
+1 = "1"
+2 = "2"
+3 = "11"
+4 = "12"
+5 = "21"
+6 = "22"
+
 */
 
-function bijective(){
-    
-}
+const bijectiveBinary = {
+  convertToInt: function (str) {
+    // enter your code here
+  },
+  convertFromInt: function (int) {
+    // enter your code here
+    let temp = num.toString().split("").reverse();
+    // console.log(temp)
+    let result = temp.reduce((sum, cur, index) => {
+      return sum + 2 ** index * cur;
+    }, 0);
+    console.log(result);
+    return result;
+  },
+};
+
