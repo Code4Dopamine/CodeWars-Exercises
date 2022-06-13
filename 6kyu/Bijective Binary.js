ed6baff77bbab0df8bcecc81242ebd84181328e9
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2022-06-13 08:55:25 +0800

    Complete Bijective Binary.js

--- 6kyu/Bijective Binary.js
+++ 6kyu/Bijective Binary.js
@@ -16,18 +16,33 @@ https://en.wikipedia.org/wiki/Bijective_numeration
 */
 
 const bijectiveBinary = {
-  convertToInt: function (str) {
-    // enter your code here
-  },
-  convertFromInt: function (int) {
-    // enter your code here
-    let temp = num.toString().split("").reverse();
-    // console.log(temp)
-    let result = temp.reduce((sum, cur, index) => {
-      return sum + 2 ** index * cur;
-    }, 0);
-    console.log(result);
-    return result;
-  },
-};
-
+    convertToInt: function (str) {
+      if (str === "") return 0;
+      let temp = str.split("").reverse();
+      // console.log(temp)
+      let result = temp.reduce((sum, cur, index) => {
+        return sum + 2 ** index * cur;
+      }, 0);
+      console.log("fromstr:"+result);
+      return result;
+    },
+    convertFromInt: function (int) {
+      // enter your code here
+      if (int === 0) return "";
+      let string = [];
+      function f(x) {
+        return Math.ceil(x) - 1;
+      }
+      let qn = f(int / 2); // q0
+      string.push(int - qn * 2); // a0
+      while (qn != 0) {
+        let qnInc = f(qn / 2);
+        let anInc = qn - qnInc * 2;
+        string.push(anInc);
+        qn = qnInc;
+      }
+      string = string.reverse().join("")
+      console.log(string)
+      return string;
+    },
+  };
