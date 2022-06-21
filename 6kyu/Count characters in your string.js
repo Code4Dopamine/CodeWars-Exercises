240ea1686ed6944bd4e2d2d02aa02852f92b3296
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2022-06-21 23:21:01 +0800

    Update Codewar

--- 6kyu/Count characters in your string.js
+++ 6kyu/Count characters in your string.js
@@ -6,22 +6,22 @@ What if the string is empty? Then the result should be empty object literal, {}.
 
 function count(str) {
   // The function code should be here
-  if(str.length === 0 || str === null) return {};
+  if (str.length === 0 || str === null) return {};
   const dic = {};
   const strArr = str.split("");
 
-  str.split("").forEach(e=> {
-        console.log(e)
-      if(Object.keys(dic).includes(e)){
-        dic[e] = 1
-      }
-      dic[e] = dic[e]+=1
+  str.split("").forEach((e) => {
+    console.log(e);
+    if (!Object.keys(dic).includes(e)) {
+      console.log("This ran");
+      dic[e] = 1;
+    } else {
+      dic[e]++;
+    }
+  });
 
-  })
-
-  console.log(dic)
+  console.log(dic);
   return dic;
 }
 
-
-count("aabbbc")
+count("aabbbc");
